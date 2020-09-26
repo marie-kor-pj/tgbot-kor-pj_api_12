7ed61d82e20a7c1835c99ed7dc26507153457ee7
JaeBeom An <33373557+NewPremium@users.noreply.github.com>
2020-09-26 19:32:53 +0900

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -6,7 +6,7 @@ from typing import Optional, List
 from telegram import Message, Chat, Update, Bot, User
 from telegram import ParseMode, InlineKeyboardMarkup, InlineKeyboardButton
 from telegram.error import Unauthorized, BadRequest, TimedOut, NetworkError, ChatMigrated, TelegramError
-from telegram.ext import CommandHandler, Filters, MessageHandler, CallbackQueryHandler, ConversationHandler
+from telegram.ext import CommandHandler, Filters, MessageHandler, CallbackQueryHandler
 from telegram.ext.dispatcher import run_async, DispatcherHandlerStop, Dispatcher
 from telegram.utils.helpers import escape_markdown
 
