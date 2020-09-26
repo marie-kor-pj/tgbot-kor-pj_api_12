29dc7b3034dd9d3da6ef4db9e3c819c8c5b43c03
JaeBeom An <33373557+NewPremium@users.noreply.github.com>
2020-09-26 13:31:49 +0900

    Update handlers.py

--- tg_bot/modules/helper_funcs/handlers.py
+++ tg_bot/modules/helper_funcs/handlers.py
@@ -33,6 +33,6 @@ class CustomCommandHandler(tg.CommandHandler):
             return False
 
 
-class CustomRegexHandler(tg.MessageHandler):
+class CustomRegexHandler(tg.RegexHandler):
     def __init__(self, pattern, callback, friendly="", **kwargs):
         super().__init__(tg.Filters.regex('pattern') , callback, **kwargs)
